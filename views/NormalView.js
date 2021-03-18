bc1d86d1685dcffeec3fe1faad3f9b973c523832
AdamKierat <adam.kierat1@gmail.com>
2021-03-18 22:22:44 +0100

    changed appi

--- views/NormalView.js
+++ views/NormalView.js
@@ -3,18 +3,23 @@ import { Platform, Text, View, StatusBar, StyleSheet, Button } from 'react-nativ
 
 
 
-const NormalView = ({temp, setTemp}) => {
+const NormalView = ({ weatherCondition, sunrise, sunset, temperature, pressure }) => {
     const styles = StyleSheet.create({
         normalView: {
             flex: 1,
             paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
         },
     });
-    
-    console.log("xd", temp)
+    // console.log("weatherCondition: ", weatherCondition)
+    // console.log("sunrise: ", sunrise)
+    // console.log("sunset: ", sunset)
+    // console.log("temperature: ", temperature)
+    // console.log("pressure: ", pressure)
+
     return (
         <View style={styles.normalView}>
-            <Text>{temp}</Text>
+            <Text>{temperature}</Text>
+
         </View>
     );
 
