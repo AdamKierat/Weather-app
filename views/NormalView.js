f8c0187a8dbc1132ca6fd3d8ed84bd14c44fc28b
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-03-29 02:02:45 +0200

    normalview + icons

--- views/NormalView.js
+++ views/NormalView.js
@@ -7,27 +7,23 @@ import {
   StyleSheet,
   TextInput,
   Image,
+  TouchableOpacity,
 } from "react-native";
 
 import { getTimeToDisplay, getCurrentDateTime } from "../utils/timeConverters";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
-import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
-import { Dimensions } from 'react-native';
-import Feather from "react-native-vector-icons/MaterialCommunityIcons";
+import Icon from "react-native-vector-icons/Feather";
+
 const NormalView = ({
-  currentlon,
-  currentlat,
   city,
   temperature,
   pressure,
   description,
-  icon,
-  country,
   sunset,
   sunrise,
   iconLink,
   setCity,
-  fetchWeather
+  fetchWeather,
 }) => {
   const [cityHelper, setCityHelper] = useState("");
   const [dateNow, setDateNow] = useState();
@@ -38,132 +34,141 @@ const NormalView = ({
 
   return (
     <View style={styles.container}>
-
-      {/* <Text>Long: {currentlon}</Text>
-      <Text>Lat: {currentlat}</Text> */}
       <View style={styles.inputboxcontainer}>
         <TextInput
+          placeholder={city}
           style={styles.textInput}
           onChangeText={(text) => setCityHelper(text)}
           onSubmitEditing={() => {
-            setCity(cityHelper)
-            fetchWeather(cityHelper)
-          }
-          }
+            setCity(cityHelper);
+            fetchWeather(cityHelper);
+          }}
         />
-        <FontAwesome
-          name="search"
-          onPress={() => fetchWeather(cityHelper)}
-          style={{ margin: 8 }}
-          color={"white"}
-          size={40} />
+        <TouchableOpacity>
+          <FontAwesome
+            name="search"
+            onPress={() => fetchWeather(cityHelper)}
+            style={{ margin: 8 }}
+            color={"white"}
+            size={40}
+          />
+        </TouchableOpacity>
       </View>
       <View style={styles.imagineHolder}>
         <Image
-          style={styles.image
-          }
+          style={styles.image}
           source={{
             uri: iconLink,
           }}
         />
       </View>
-
       <View style={styles.infobox}>
-        <Text style={{
-          paddingTop: 15,
-          textAlign: 'center',
-          fontSize: 20,
-          fontWeight: "bold",
-          color: "#ffffff",
-        }}>{dateNow}</Text>
-
-        <View style={{
-          flex: 1,
-          flexDirection: "row",
-          paddingTop: 50,
-
-        }}>
-          <Text style={{
-            paddingLeft: 30,
-            fontSize: 65,
+        <Text
+          style={{
+            paddingTop: 15,
+            textAlign: "center",
+            fontSize: 20,
             fontWeight: "bold",
             color: "#ffffff",
+          }}
+        >
+          {dateNow}
+        </Text>
 
-          }}>{Math.round(temperature)} °  </Text>
-          <Text style={{
-            paddingTop: 20,
-            fontSize: 30,
-            fontWeight: "bold",
-            color: "#ffffff",
-
-
-          }}>{description}</Text>
-        </View>
-        <View style={{
-          flex: 1,
-          flexDirection: "row",
-          paddingTop: 50,
-
-        }}>
-          <FontAwesome5
-            name="wind"
-            color="white"
-            size={30}
-            style={{ paddingLeft: 25, }}
-          />
-          <Text style={{
-
-            fontSize: 25,
-            fontWeight: "bold",
-            color: "#ffffff",
-
-          }}> Pressure   |</Text>
-
-          <Text style={{
-
-            fontSize: 25,
-            fontWeight: "bold",
-            color: "#ffffff",
-            paddingLeft: 20
-          }}>{pressure} hPa</Text>
-
-        </View>
-      </View>
-
-      <View style={{
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "flex-end",
-
-      }}>
-        <View style={styles.suninfo}>
-          <Image
-            style={styles.sunriseinfoimagine}
-            source={{
-              uri: "https://pngimg.com/uploads/sun/sun_PNG13427.png",
-            }}>
-          </Image>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            paddingTop: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Text
-            style={styles.suninfoText} >
-            {getTimeToDisplay(new Date(sunrise * 1000))}</Text>
-
-        </View>
-        <View style={{ paddingLeft: 120, alignItems: 'center', }}>
-          <Image
-            style={styles.sunriseinfoimagine}
-            source={{
-              uri: "https://assets.stickpng.com/images/580b585b2edbce24c47b270b.png",
-            }}></Image>
+            style={{
+              paddingLeft: 30,
+              fontSize: 65,
+              fontWeight: "bold",
+              color: "#ffffff",
+            }}
+          >
+            {Math.round(temperature)} °{" "}
+          </Text>
           <Text
-            style={styles.suninfoText} >
-            {getTimeToDisplay(new Date(sunset * 1000))}
+            style={{
+              paddingTop: 20,
+              fontSize: 30,
+              fontWeight: "bold",
+              color: "#ffffff",
+              paddingRight: 20,
+            }}
+          >
+            {description}
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            paddingTop: 50,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 25,
+              fontWeight: "bold",
+              color: "#ffffff",
+              alignContent: "center",
+              justifyContent: "center",
+            }}
+          >
+            <View
+              style={{
+                transform: [{ rotate: "270deg" }],
+                marginLeft: 5,
+                marginTop: 5,
+              }}
+            >
+              <Icon name="skip-back" color="#FFFFFF" size={23} />
+            </View>{" "}
+            Pressure{"  "}|
+          </Text>
+          <Text
+            style={{
+              fontSize: 25,
+              fontWeight: "bold",
+              color: "#ffffff",
+              paddingLeft: 20,
+            }}
+          >
+            {pressure} hPa
           </Text>
         </View>
       </View>
 
-    </View >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "flex-end",
+        }}>
+        <View style={styles.suninfo}>
+          <Icon name="sunrise" color="#FFFFFF" size={49} />
+          <Text style={styles.suninfoText}>
+            {getTimeToDisplay(new Date(sunrise * 1000))}
+          </Text>
+        </View>
+        <View style={{ paddingLeft: 120, alignItems: "center" }}>
+          <Icon name="sunset" color="#FFFFFF" size={49} />
+          <Text style={styles.suninfoText}>
+            {getTimeToDisplay(new Date(sunset * 1000))}
+          </Text>
+        </View>
+      </View>
+    </View>
   );
-
 };
 
 const styles = StyleSheet.create({
@@ -171,38 +176,35 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     backgroundColor: "#2d9cdb",
     flex: 1,
-
   },
   textInput: {
     height: 50,
     width: 320,
     margin: 5,
-    borderColor: "#ffffff",
+    borderColor: "rgba(158, 150, 150, 0)",
     borderRadius: 20,
     borderWidth: 3.5,
     textAlign: "center",
     fontSize: 30,
-    fontWeight: "bold",
     color: "#ffffff",
     textTransform: "uppercase",
+    fontFamily: "Overpass-Light",
+    fontWeight: "bold",
   },
   image: {
     width: 400,
     height: 160,
     aspectRatio: 1.9,
     paddingBottom: 10,
-
-
   },
   inputboxcontainer: {
     flexDirection: "row",
     padding: 10,
 
-    height: 75
+    height: 75,
   },
   imagineHolder: {
-
-    alignItems: 'center',
+    alignItems: "center",
     height: 150,
   },
   infobox: {
@@ -216,7 +218,7 @@ const styles = StyleSheet.create({
   },
   suninfo: {
     paddingLeft: 60,
-    alignItems: 'center',
+    alignItems: "center",
   },
   suninfoText: {
     fontSize: 30,
@@ -227,7 +229,7 @@ const styles = StyleSheet.create({
     height: 60,
     width: 100,
     aspectRatio: 1.3,
-  }
-})
+  },
+});
 
 export default NormalView;
